961869c638d68470277c3489e484c15089fd6f33
Mswork6 <denis.ch74@mail.ru>
2023-10-29 19:46:10 +0500

    feat: updated Task1

--- src/main/java/edu/hw3/Task1.java
+++ src/main/java/edu/hw3/Task1.java
@@ -1,16 +1,13 @@
 package edu.hw3;
 
-
 import java.util.HashMap;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class Task1 {
     private static final HashMap<Character, Character> HASH_MAP = new HashMap<>();
-
     private static final int AMOUNT_OF_LETTERS = 26;
     private static final int START_POS_CAPITAL_LETTERS = 65;
-
     private static final int START_POS_LOWERCASE_LETTERS = 97;
     private static final int DISTANCE = 25;
 
